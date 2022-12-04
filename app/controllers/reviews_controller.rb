951f0531e4874a7fdb321e002284d84224a260a5
sean-code <john.nganga@student.moringaschool.com>
2022-12-04 21:09:33 +0300

    Create the CRUD actions of the Review Model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,47 @@
 class ReviewsController < ApplicationController
+    #All Cars
+    def index 
+        @reviews=Review.all
+        render json: @reviews, status: :ok
+    end
+    
+    #GET /cars/ 1
+    def show 
+        if set_review
+            render json: set_review, status: :ok
+        else
+           render json: {error: "Review Does Not Exist"}, status: :not_found
+        end
+    end
+
+    #CREATE
+    def create 
+        @review = Review.new(review_params)
+        if @review.save
+            render json: @review, status: :created 
+        else 
+            render json: {error: "Review Not Updated"}, status: :unprocessable_entity
+    end
+
+    #PATCH & PUT
+    def update 
+        @review=Review.update(review_params)
+        if @review
+            render json: @review, status: :ok
+        else 
+            render json: {error: "Not Updated"}, status: :unprocessable_entity
+    end
+
+
+    private 
+    def set_review
+        review = Review.find_by(id: params[:id])
+    end
+
+    def review_params 
+        params.require(:review).permit(:username, :rating, :comment)
+    end
+
+
+
 end
