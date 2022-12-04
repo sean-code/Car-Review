97eb61731d3f073e3a8509a5f06b6b5d4250fec5
sean-code <john.nganga@student.moringaschool.com>
2022-12-04 19:20:53 +0300

    Edit the column names

--- db/migrate/20221204102259_create_cars.rb
+++ db/migrate/20221204102259_create_cars.rb
@@ -1,15 +1,15 @@
 class CreateCars < ActiveRecord::Migration[7.0]
   def change
     create_table :cars do |t|
-      t.string :manufacture
-      t.string :make_and_model
-      t.string :color
-      t.string :image_url
+      t.string :price
+      t.string :make
+      t.string :model
       t.string :fuel_type
       t.string :transmission
       t.integer :mileage
       t.integer :engine_size
       t.integer :year
+      t.string :image
 
       t.timestamps
     end
