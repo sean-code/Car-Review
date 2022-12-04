7514c1d933254f37a2ba365072e10147bf770a0d
sean-code <john.nganga@student.moringaschool.com>
2022-12-04 21:08:59 +0300

    Debug a typoError

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -18,7 +18,7 @@ class CarsController < ApplicationController
     def create
         @car = Car.new(car_params)
         if @car.save
-          render json: @car, status: :created, location: @car
+          render json: @car, status: :created
         else
           render json: @car.errors, status: :unprocessable_entity
         end
