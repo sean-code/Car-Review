51f55cb716519beca69def80955d64ffdbd18600
sean-code <john.nganga@student.moringaschool.com>
2022-12-05 08:52:02 +0300

    Label my code using comments and riddance of typoError

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
-class SessionsController < ApplicationController
+class SessionsController < ApplicationController.
+
+    #Login
     def create
         user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
@@ -9,8 +11,10 @@ class SessionsController < ApplicationController
         end
       end
     
+      #Logout
       def destroy
         session.delete :user_id
         head :no_content
       end
+      
 end
