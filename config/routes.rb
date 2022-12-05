429c913ca81fc912726c8a8d94957dcfa3483c8e
sean-code <john.nganga@student.moringaschool.com>
2022-12-05 08:29:55 +0300

    Create sessions routes for the login and logout functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :cars
   resources :reviews
 
+  #User sign up
   post '/signup', to: "users#create"
   get '/me', to: "users#show"
+
+  #Sessions
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
